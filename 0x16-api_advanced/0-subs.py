7d0cb8d59531ce3b419b2ae4b5364e6168ab0f19
Nii-101 <noelamptey101@gmail.com>
2024-03-07 23:34:52 +0000

    Fixed task 0 and task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""
-0-subs
-"""
+'''
+This function queries the Reddit API and prints the
+titles of the first 10 hot posts
+'''
+
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given
-    subreddit. If the subreddit is invalid, return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom-agent'}
+def top_ten(subreddit):
+    '''Returns top ten posts titles'''
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Alx-User'}
+    params = {'limit': 10}
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json().get('data', {})
-            subscribers = data.get('subscribers', 0)
-            return (subscribers)
-        else:
-            return (0)
-    except requests.RequestException as e:
-        return (0)
+    if response.status_code == 200:
+        data = response.json()
+        objects = data['data']['children']
+        for obj in objects:
+            print(obj['data']['title'])
+    else:
+        print('None')
