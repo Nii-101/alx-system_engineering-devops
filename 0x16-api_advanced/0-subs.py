54c3246a92e8a3ed8d9580f73b749d684299def4
Nii-101 <noelamptey101@gmail.com>
2024-03-07 23:21:18 +0000

    fixed task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+0-subs
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'custom-agent'}
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    """
+    Queries the Reddit API and returns the number of subscribers for a given
+    subreddit. If the subreddit is invalid, return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-agent'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get('data', {})
+            subscribers = data.get('subscribers', 0)
+            return (subscribers)
+        else:
+            return (0)
+    except requests.RequestException as e:
+        return (0)
